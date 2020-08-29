a36720ef761a48d7852bdfbf8bafcd51864502a1
Henry Hsu <hhsu@henryh-mac.lan>
2020-08-29 13:31:00 -0700

    Updates

---
+++ precache-manifest.ba235dd7221d80a7dc3defc7630a338c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4224cce8547b2bc6eaa591f54536e0b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dba62cecef90208494ee",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba62cecef90208494ee",
+    "url": "/portfolio/static/js/main.77fccd17.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
